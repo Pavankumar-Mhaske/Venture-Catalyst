49233e148a82510482f68912cb4d3e14b1f0e9c5
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-06 00:34:03 +0530

    chore: imported the styling file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import { Sepolia } from "@thirdweb-dev/chains";
 import App from "./App";
-
+import './index.css';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
