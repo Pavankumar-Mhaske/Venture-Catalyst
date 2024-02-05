e75b5e4ef5b268688099872f6ea9a1e0d1cea20d
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-06 00:33:44 +0530

    chore: started creating the app.js file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+      <div className="sm:felx hidden mr-10 relative">Sidebar</div>
+      <div className="flex-1 max-sm:w-full max-w[1280px] mx-auto sm:pr-5">
+        Navbar
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} /> */}
+        </Routes>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
