5987597fe60bad3e6333356f8872c721f27a600c
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-06 00:35:01 +0530

    feat: created the all pages

---
+++ client/src/pages/CampaignDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CampaignDetails = () => {
+  return (
+    <div>CampaignDetails</div>
+  )
+}
+
+export default CampaignDetails
---
+++ client/src/pages/CreateCampaign.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateCampaign = () => {
+  return (
+    <div>CreateCampaign</div>
+  )
+}
+
+export default CreateCampaign
