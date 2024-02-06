857698bcfc04ddba7222358a7d8711b54d291f91
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-06 18:22:13 +0530

    chore: address is fetched from useStateContext

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-// import { useStateContext } from "../context";
+import { useStateContext } from "../context";
 import { CustomButton } from "./";
 import { logo, menu, search, thirdweb } from "../assets";
 import { navlinks } from "../constants";
@@ -10,9 +10,9 @@ const Navbar = () => {
   const navigate = useNavigate();
   const [isActive, setIsActive] = useState("dashboard");
   const [toggleDrawer, setToggleDrawer] = useState(false);
-  // const { connect, address } = useStateContext();
+  const { connect, address } = useStateContext();
 
-  const address = '0x1234567890123456789012345678901234567890';
+  // const address = '0x1234567890123456789012345678901234567890';
   return (
     <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
       <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
