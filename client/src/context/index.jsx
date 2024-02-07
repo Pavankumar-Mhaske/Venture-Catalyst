c51866edb1ef98a7b502e62c78df3a67c7e1dc9c
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-07 18:16:44 +0530

    chore: created getCampaigns functions in context

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -41,9 +41,34 @@ export const StateContextProvider = ({ children }) => {
     }
   };
 
+  const getCampaigns = async () => {
+    const campaigns = await contract.call("getCampaigns");
+
+    const parsedCampaigns = campaigns.map((campaign, i) => ({
+      owner: campaign.owner,
+      title: campaign.title,
+      description: campaign.description,
+      target: ethers.utils.formatEther(campaign.target.toString()),
+      deadline: campaign.deadline.toString(),
+      amountCollected: ethers.utils.formatEther(
+        campaign.amountCollected.toString()
+      ),
+      image: campaign.image,
+      pId: i,
+    }));
+    console.log("Campaigns", parsedCampaigns);
+    return parsedCampaigns;
+  };
+
   return (
     <StateContext.Provider
-      value={{ address, contract, connect, createCampaign: publishCampaign }}
+      value={{
+        address,
+        contract,
+        connect,
+        createCampaign: publishCampaign,
+        getCampaigns,
+      }}
     >
       {children}
     </StateContext.Provider>
