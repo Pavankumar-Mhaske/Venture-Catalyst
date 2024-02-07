faa137a31570d4bdb920a76c6abeec03509ed1f2
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-08 00:08:07 +0530

    chore: created the loader component

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { loader } from "../assets";
 
 const Loader = () => {
   return (
-    <div>Loader</div>
-  )
-}
+    <div className="fixed inset-0 z-10 h-screen bg-[rgba(0,0,0,0.7)] flex items-center justify-center flex-col">
+      <img
+        src={loader}
+        alt="loader"
+        className="w-[100px] h-[100px] object-contain"
+      />
+      <p className="mt-[20px] font-epilogue font-bold text-[20px] text-white text-center">
+        Transaction is in progress <br />
+        Please wait...
+      </p>
+    </div>
+  );
+};
 
-export default Loader
+export default Loader;
