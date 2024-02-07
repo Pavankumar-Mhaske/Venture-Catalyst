f6c9c4568a3e63108921184fa034419554ea63ac
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-07 18:17:03 +0530

    feat: created the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useStateContext } from "../context";
+import { DisplayCampaigns } from "../components";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
+  const { address, contract, getCampaigns } = useStateContext();
 
-export default Home
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+    const data = await getCampaigns();
+    setCampaigns(data);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    if (contract) {
+      fetchCampaigns();
+    }
+  }, [address, contract]);
+
+  return (
+    <DisplayCampaigns
+      title="All Campaigns"
+      isLoading={isLoading}
+      campaigns={campaigns}
+    />
+  );
+};
+
+export default Home;
