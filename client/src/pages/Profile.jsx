d8ac6123125d589bc6c0016414b4b6b8ddf97ab3
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-07 18:27:34 +0530

    feat: created the profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useStateContext } from "../context";
+import { DisplayCampaigns } from "../components";
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
+  const { address, contract, getUserCampaigns } = useStateContext();
 
-export default Profile
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+    const data = await getUserCampaigns();
+    setCampaigns(data);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    if (contract) {
+      fetchCampaigns();
+    }
+  }, [address, contract]);
+
+  return (
+    <DisplayCampaigns
+      title="All Campaigns"
+      isLoading={isLoading}
+      campaigns={campaigns}
+    />
+  );
+};
+
+export default Profile;
