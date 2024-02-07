64df9c92522f2fd70e3def52a10c465fb4a27754
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-07 23:47:08 +0530

    chore: created the donate + getDonations  functionalities

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -70,6 +70,29 @@ export const StateContextProvider = ({ children }) => {
     return filteredCampaigns;
   };
 
+  const donate = async (pId, amount) => {
+    const data = await contract.call("donateToCampaign", [pId], {
+      value: ethers.utils.parseEther(amount),
+    });
+    return data;
+  };
+
+  const getDonations = async (pId) => {
+    const donations = await contract.call("getDonators", [pId]);
+    const numberOfDonations = donations[0].length;
+
+    const parsedDonations = [];
+
+    for (let i = 0; i < numberOfDonations; i++) {
+      parsedDonations.push({
+        donator: donations[0][i],
+        donation: ethers.utils.formatEther(donations[1][i].toString()),
+      });
+    }
+
+    return parsedDonations;
+  };
+
   return (
     <StateContext.Provider
       value={{
@@ -79,6 +102,8 @@ export const StateContextProvider = ({ children }) => {
         createCampaign: publishCampaign,
         getCampaigns,
         getUserCampaigns,
+        donate,
+        getDonations,
       }}
     >
       {children}
