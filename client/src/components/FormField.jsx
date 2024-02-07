d52582206ba2a680526fa47597b1bd5f2a73346a
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-07 23:45:44 +0530

    chore: resolve the bug of being type string to number

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -20,7 +20,6 @@ const FormField = ({
           required
           value={value}
           onChange={handleChange}
-          type={inputType}
           rows={10}
           placeholder={placeholder}
           className="p-[15px] sm:px-[25px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[14px] placeholder:text-[#4b5264] rounded-[10px] sm:min-w-[300px]"
@@ -35,6 +34,15 @@ const FormField = ({
           placeholder={placeholder}
           className="p-[15px] sm:px-[25px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[14px] placeholder:text-[#4b5264] rounded-[10px] sm:min-w-[300px]"
         />
+
+        // <input
+        //         type="number"
+        //         placeholder="ETH 0.1"
+        //         step="0.001"
+        //         className="w-full py-[10px] sm:px-[20px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[18px] leading-[30px] placeholder:text-[#4b5264] rounded-[10px]"
+        //         value={amount}
+        //         onChange={(e) => setAmount(e.target.value)}
+        //       />
       )}
     </label>
   );
