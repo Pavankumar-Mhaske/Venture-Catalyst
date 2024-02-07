8b9f75c96129530124d9429dc7c2d934b0311a01
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-07 18:17:32 +0530

    chore: there is the problem in deadline value

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,23 +1,24 @@
 export const daysLeft = (deadline) => {
-    const difference = new Date(deadline).getTime() - Date.now();
-    const remainingDays = difference / (1000 * 3600 * 24);
-  
-    return remainingDays.toFixed(0);
-  };
-  
+  // console.log("deadline", new Date(deadline).getTime());
+  const difference = new Date(deadline).getTime() - Date.now();
+  const remainingDays = difference / (1000 * 3600 * 24);
+
+  // return 4;
+  return remainingDays.toFixed(0);
+};
+
 export const calculateBarPercentage = (goal, raisedAmount) => {
-    const percentage = Math.round((raisedAmount * 100) / goal);
-  
-    return percentage;
-  };
-  
+  const percentage = Math.round((raisedAmount * 100) / goal);
+
+  return percentage;
+};
+
 export const checkIfImage = (url, callback) => {
-    const img = new Image();
-    img.src = url;
-  
-    if (img.complete) callback(true);
-  
-    img.onload = () => callback(true);
-    img.onerror = () => callback(false);
-  };
-  
+  const img = new Image();
+  img.src = url;
+
+  if (img.complete) callback(true);
+
+  img.onload = () => callback(true);
+  img.onerror = () => callback(false);
+};
