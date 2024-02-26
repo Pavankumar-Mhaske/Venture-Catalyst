f13df422f702d2f543657eb47eebe1bd999e78c8
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-02-26 16:04:34 +0530

    chore: changed the negative dating

--- client/src/components/FundCard.jsx
+++ client/src/components/FundCard.jsx
@@ -58,7 +58,7 @@ const FundCard = ({
           </div>
           <div className="flex flex-col">
             <h4 className="font-epilogue font-semibold text-[14px] text-[#b2b3bd] leading-[22px] ">
-              {remainingDays}
+              {remainingDays < 1 ? 0 : remainingDays}
             </h4>
             <p className="mt-[3px] font-epilogue font-normal text-[12px] leading-[18px] text-[#808191] sm:max-w-[120px] truncate ">
               Days Left{""}
